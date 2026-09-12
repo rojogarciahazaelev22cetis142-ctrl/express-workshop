eedffee12b169fa29e76cda1bf62a8c917db1b88
Christopher Rojo <rojo.garcia.hazael.ev22.cetis142@gmail.com>
2026-09-11 21:13:50 -0600

    Update fales and add body-parser

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
     const express = require('express');
+    const bodyparser = require('body-parser');
     const app = express();
     const { pokemon } = require('./pokedex.json')
+    
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
     /*
+
     verbos HTTP
-    GET: Solicita datos de un recurso específico.
-    POST: Envía datos para crear un nuevo recurso.
-    PUT: Actualiza un recurso existente con nuevos datos.
+    GET: obtener recursos
+    POST: almacenar un recurso
+    PUT: modificar un recurso
     DELETE: Elimina un recurso específico.
-    PATCH: Actualiza parcialmente un recurso existente.
+    PATCH: modificar una parte de un recurso ya existente.
 
     */
     app.get("/", (req, res, next) =>{
-        res.status(200);
-        res.send("Bienvenido al Pokedex"); 
+        return res.status(200).send("Bienvenido al Pokedex"); 
     })
     /*Como obtener parametro de la url */
-    app.get("/:pokemon/all", (req, res, next) =>{
-        res.status(200); 
-        res.send(pokemon);
+    app.get("/pokemon", (req, res, next) =>{
+        return res.status(200).send(pokemon);
     });
+
+    app.post('/pokemon', (req, res, next) => {
+        res.status(200).send(req.body);
+    }); 
 
     /*Lo que sigue despues del id es un RegEx que funciona como una condicional para los parametros recibidos por el usuario */
     app.get('/pokemon/:id', (req, res, next) => {
@@ -27,31 +34,28 @@
             return next();
         }
         else{
-            if( id >= 0 && id <= 150)
-            {
-                res.status(200);
-                return res.send(pokemon[req.params.id - 1]);   
-            }
-            res.status(404);
-            res.send("Pokemon no encontrado");
+            ( id >= 0 && id <= 150) ?
+            res.status(200).send(pokemon[req.params.id - 1]) :
+            res.status(404).send("Pokemon no encontrado");
         }
         
     })
-
     app.get('/pokemon/:name', (req, res, next) => {
-        for(let i = 0; i < pokemon.length; i++){
-            if( pokemon[i].name == req.params.name){
-                res.status(200);
-                res.send(pokemon[i]);
-            } 
-        }
-        res.status(404);
-        res.send("Pokemon no encontrado");
+        const name = req.params.name;
+        
+        (!/^[A-Za-z]+$/.test(name)) ? res.status(400).send("Pokemon no encontrado") : null;
+        
+        const pk = pokemon.filter((p) => {
+            return(p.name.toUpperCase() == name.toUpperCase()) && p;
+        }); 
+        (pk.length > 0) ? res.status(200).send(pk[0]) : res.status(404).send("Pokemon no encontrado");
     }) 
 
     app.listen(process.env.PORT || 3000,()  => {  
         console.log('Server is running on port 3000');
     });
+
+
 
 
 
